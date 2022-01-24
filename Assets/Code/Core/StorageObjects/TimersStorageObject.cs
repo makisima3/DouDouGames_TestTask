d3b24006316700030794cfe1887c2ef9da337ce9
maxim <maksimya@bk.ru>
2022-01-24 18:11:26 +0300

    Fixes

--- Assets/Code/Core/StorageObjects/TimersStorageObject.cs
+++ Assets/Code/Core/StorageObjects/TimersStorageObject.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using PersistentStorage;
-using TMPro.EditorUtilities;
 using UnityEngine;
 
 namespace Code.Core.StorageObjects
--- Assets/Code/Core/TimerHolder.cs
+++ Assets/Code/Core/TimerHolder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Code.Core.StorageObjects;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace Code.Core
